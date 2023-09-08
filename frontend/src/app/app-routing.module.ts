c734dc56ce5e72131b269024f46f60226646cebe
Billy Li <billy1999fans@gmail.com>
2023-09-08 18:03:01 +0200

    Add login page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SigninComponent } from './components/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: LoginComponent,
+    path: ''
+  },
+  {
+    component: RegisterComponent,
+    path: 'register'
+  },
+  {
+    component: SigninComponent,
+    path: 'signin'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
