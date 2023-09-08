642571a2b16ef2553f26cf23fede2b0142d101e2
Billy Li <billy1999fans@gmail.com>
2023-09-08 15:57:53 +0200

    Reset app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components//header/header.component';
-import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
