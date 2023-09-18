e747e70a3105e31788489583fa7e592b7e19e12f
= <=>
2023-09-18 15:12:04 +0200

    Update comments

--- frontend/src/app/auth/guards/auth.guard.ts
+++ frontend/src/app/auth/guards/auth.guard.ts
@@ -1,4 +1,4 @@
-// auth.guard.ts
+// Import necessary modules and decorators from Angular.
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
@@ -10,14 +10,15 @@ export class AuthGuard implements CanActivate {
 
   canActivate(): boolean {
     // Check if the user is logged in (you can implement your logic here)
-    const isLoggedIn = localStorage.getItem('User');
+    const isLoggedIn = localStorage.getItem('User'); // Retrieve user login status from local storage.
 
     if (!isLoggedIn) {
-      // If not logged in, redirect to the login page
-      this.router.navigate(['']);
-      return false; // Prevent navigation to the requested route
+      // If not logged in, redirect to the login page.
+      this.router.navigate(['']); // Navigate to the root path (login page).
+      return false; // Prevent navigation to the requested route.
     }
 
-    return true; // Allow navigation to the requested route
+    // If the user is logged in, allow navigation to the requested route.
+    return true;
   }
 }
